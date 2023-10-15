c058186cf4053fd2aa03cc7937796e054bd94cb4
clarspau <125636195+clarspau@users.noreply.github.com>
2023-10-14 20:15:20 -0700

    modified air_traffic.sql to create better tables

--- air_traffic.sql
+++ air_traffic.sql
@@ -7,31 +7,64 @@ CREATE DATABASE air_traffic;
 
 \c air_traffic
 
-CREATE TABLE tickets
+CREATE TABLE passengers
 (
-  id SERIAL PRIMARY KEY,
+  passenger_id SERIAL PRIMARY KEY,
   first_name TEXT NOT NULL,
-  last_name TEXT NOT NULL,
-  seat TEXT NOT NULL,
-  departure TIMESTAMP NOT NULL,
-  arrival TIMESTAMP NOT NULL,
-  airline TEXT NOT NULL,
-  from_city TEXT NOT NULL,
-  from_country TEXT NOT NULL,
-  to_city TEXT NOT NULL,
-  to_country TEXT NOT NULL
+  last_name TEXT NOT NULL
 );
 
-INSERT INTO tickets
-  (first_name, last_name, seat, departure, arrival, airline, from_city, from_country, to_city, to_country)
+CREATE TABLE flights
+(
+  flight_id SERIAL PRIMARY KEY,
+  airline_name TEXT NOT NULL,
+  departure_city TEXT NOT NULL,
+  departure_country TEXT NOT NULL,
+  arrival_city TEXT NOT NULL,
+  arrival_country TEXT NOT NULL
+);
+
+CREATE TABLE flight_tickets
+(
+  ticket_id SERIAL PRIMARY KEY,
+  passenger_id INT REFERENCES passengers(passenger_id),
+  flight_id INT REFERENCES flights(flight_id),
+  seat_number TEXT NOT NULL,
+  departure_time TIMESTAMP NOT NULL,
+  arrival_time TIMESTAMP NOT NULL
+);
+
+INSERT INTO passengers (first_name, last_name)
 VALUES
-  ('Jennifer', 'Finch', '33B', '2018-04-08 09:00:00', '2018-04-08 12:00:00', 'United', 'Washington DC', 'United States', 'Seattle', 'United States'),
-  ('Thadeus', 'Gathercoal', '8A', '2018-12-19 12:45:00', '2018-12-19 16:15:00', 'British Airways', 'Tokyo', 'Japan', 'London', 'United Kingdom'),
-  ('Sonja', 'Pauley', '12F', '2018-01-02 07:00:00', '2018-01-02 08:03:00', 'Delta', 'Los Angeles', 'United States', 'Las Vegas', 'United States'),
-  ('Jennifer', 'Finch', '20A', '2018-04-15 16:50:00', '2018-04-15 21:00:00', 'Delta', 'Seattle', 'United States', 'Mexico City', 'Mexico'),
-  ('Waneta', 'Skeleton', '23D', '2018-08-01 18:30:00', '2018-08-01 21:50:00', 'TUI Fly Belgium', 'Paris', 'France', 'Casablanca', 'Morocco'),
-  ('Thadeus', 'Gathercoal', '18C', '2018-10-31 01:15:00', '2018-10-31 12:55:00', 'Air China', 'Dubai', 'UAE', 'Beijing', 'China'),
-  ('Berkie', 'Wycliff', '9E', '2019-02-06 06:00:00', '2019-02-06 07:47:00', 'United', 'New York', 'United States', 'Charlotte', 'United States'),
-  ('Alvin', 'Leathes', '1A', '2018-12-22 14:42:00', '2018-12-22 15:56:00', 'American Airlines', 'Cedar Rapids', 'United States', 'Chicago', 'United States'),
-  ('Berkie', 'Wycliff', '32B', '2019-02-06 16:28:00', '2019-02-06 19:18:00', 'American Airlines', 'Charlotte', 'United States', 'New Orleans', 'United States'),
-  ('Cory', 'Squibbes', '10D', '2019-01-20 19:30:00', '2019-01-20 22:45:00', 'Avianca Brasil', 'Sao Paolo', 'Brazil', 'Santiago', 'Chile');
+  ('Jennifer', 'Finch'),
+  ('Thadeus', 'Gathercoal'),
+  ('Sonja', 'Pauley'),
+  ('Waneta', 'Skeleton'),
+  ('Berkie', 'Wycliff'),
+  ('Alvin', 'Leathes'),
+  ('Cory', 'Squibbes');
+
+INSERT INTO flights (airline_name, departure_city, departure_country, arrival_city, arrival_country)
+VALUES
+  ('United Airlines', 'Washington, DC', 'United States', 'Seattle', 'United States'),
+  ('British Airways', 'Tokyo', 'Japan', 'London', 'United Kingdom'),
+  ('Delta Air Lines', 'Los Angeles', 'United States', 'Las Vegas', 'United States'),
+  ('TUI Fly Belgium', 'Paris', 'France', 'Casablanca', 'Morocco'),
+  ('Air China', 'Dubai', 'UAE', 'Beijing', 'China'),
+  ('United Airlines', 'New York', 'United States', 'Charlotte', 'United States'),
+  ('American Airlines', 'Cedar Rapids', 'United States', 'Chicago', 'United States'),
+  ('American Airlines', 'Charlotte', 'United States', 'New Orleans', 'United States'),
+  ('Avianca Brasil', 'Sao Paolo', 'Brazil', 'Santiago', 'Chile');
+  
+INSERT INTO flight_tickets (passenger_id, flight_id, seat_number, departure_time, arrival_time)
+VALUES
+  (1, 1, '33B', '2018-04-08 09:00:00', '2018-04-08 12:00:00'),
+  (2, 2, '8A', '2018-12-19 12:45:00', '2018-12-19 16:15:00'),
+  (3, 3, '12F', '2018-01-02 07:00:00', '2018-01-02 08:03:00'),
+  (1, 4, '20A', '2018-04-15 16:50:00', '2018-04-15 21:00:00'),
+  (4, 5, '23D', '2018-08-01 18:30:00', '2018-08-01 21:50:00'),
+  (2, 6, '18C', '2018-10-31 01:15:00', '2018-10-31 12:55:00'),
+  (5, 7, '9E', '2019-02-06 06:00:00', '2019-02-06 07:47:00'),
+  (6, 8, '1A', '2018-12-22 14:42:00', '2018-12-22 15:56:00'),
+  (5, 9, '32B', '2019-02-06 16:28:00', '2019-02-06 19:18:00'),
+  (7, 9, '10D', '2019-01-20 19:30:00', '2019-01-20 22:45:00');
